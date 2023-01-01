d7d377bcb52482e35c131b092fd7d3a07bb8d6c0
ZaccheoMalachi <malachizaccheo@gmail.com>
2023-01-01 19:17:00 +0700

    RANGOLI AND MODULE

---
+++ Modules.py
@@ -0,0 +1,6 @@
+import math
+import datetime
+import time
+print(dir(math))
+print(time.time())
+print(dir(datetime))
---
+++ Rangoli.py
@@ -0,0 +1,13 @@
+from turtle import *
+speed(0)
+# for i in range(240):
+#     # forward(200)
+#     circle(210,180)
+#     left(643)
+for h in range(100):
+    for j in range(5):
+        forward(50)
+        right(144)
+        forward(50)
+        right(72-144)
+    left(21)
