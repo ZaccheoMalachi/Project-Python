20be1d6a0b28d6f965cfdf5d43816c0100414aec
ZaccheoMalachi <malachizaccheo@gmail.com>
2023-01-13 21:13:10 +0700

    doraemon and loop

---
+++ LoopingPattern.py
@@ -0,0 +1,21 @@
+# for i in range(1,11):
+#     print(i)
+# for i in range(1,11):
+#     print(i,end='*')
+# word=input('word:')
+# length=len(word)
+# for i in range(0,length):
+#     print(word[0:i+1])
+# for i in range(0,length):
+#     for j in range(0,i+1):
+#         print(word[i],end=' ')
+#     print()
+# for i in range(0,length):
+#     print(end=' '*(length-i))
+#     print(word[0:i+1])
+line=int(input('line:'))
+for i in range(0,line):
+    print(' '*(line-i),'* '*(i+1))
+# def sum(a,b):
+#     return sum(a,b)
+# sum(4,4)
