954b9b08ed2254a24dee4fe8c0dd71a025631580
ZaccheoMalachi <malachizaccheo@gmail.com>
2023-01-22 18:54:50 +0700

    filehandling

---
+++ filehandling.py
@@ -0,0 +1,14 @@
+# # jimn=open('file.txt','r')
+# # print(jimn.read())
+# # jimn=open('file.txt','a')
+# # jimn.write('\ncool')
+# # print(jimn.read())
+# jimn=open('file.txt','r')
+# # while(True):
+# #     text=jimn.readline()
+# #     if text!='':
+# #          print(text,end="")
+# #     else:
+# #         break
+# print(jimn.readlines())
+jimn=open('file2.txt','x')
